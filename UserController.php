9c9a0409fc01d8fc93e81b8c3fb6baf58d4b64a6
Sood122 <108793220+Sood122@users.noreply.github.com>
2023-02-17 09:50:41 +0530

    Add files via upload

---
+++ UserController.php
@@ -0,0 +1,155 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller;
+use App\Controller\AppController;
+use Cake\Event\Event;
+
+
+/**
+ * User Controller
+ *
+ * @property \App\Model\Table\UserTable $User
+ * @method \App\Model\Entity\User[]|\Cake\Datasource\ResultSetInterface paginate($object = null, array $settings = [])
+ */
+class UserController extends AppController
+{
+    
+    /**
+     * Index method
+     *
+     * @return \Cake\Http\Response|null|void Renders view
+     */
+    public function index()
+    {
+        $user = $this->paginate($this->User);
+
+        $this->set(compact('user'));
+    }
+
+    /**
+     * View method
+     *
+     * @param string|null $id User id.
+     * @return \Cake\Http\Response|null|void Renders view
+     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+     */
+    public function view($id = null)
+    {
+        $user = $this->User->get($id, [
+            'contain' => [],
+        ]);
+
+        $this->set(compact('user'));
+    }
+
+    /**
+     * Add method
+     *
+     * @return \Cake\Http\Response|null|void Redirects on successful add, renders view otherwise.
+     */
+    public function add()
+    {
+        $user = $this->User->newEmptyEntity();
+        if ($this->request->is('post')) {
+            $user = $this->User->patchEntity($user, $this->request->getData());
+            if ($this->User->save($user)) {
+                $this->Flash->success(__('The user has been saved.'));
+
+                return $this->redirect(['action' => 'index']);
+            }
+            $this->Flash->error(__('The user could not be saved. Please, try again.'));
+        }
+        $this->set(compact('user'));
+    }
+
+    /**
+     * Edit method
+     *
+     * @param string|null $id User id.
+     * @return \Cake\Http\Response|null|void Redirects on successful edit, renders view otherwise.
+     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+     */
+    public function edit($id = null)
+    {
+        $user = $this->User->get($id, [
+            'contain' => [],
+        ]);
+        if ($this->request->is(['patch', 'post', 'put'])) {
+            $user = $this->User->patchEntity($user, $this->request->getData());
+            if ($this->User->save($user)) {
+                $this->Flash->success(__('The user has been saved.'));
+
+                return $this->redirect(['action' => 'index']);
+            }
+            $this->Flash->error(__('The user could not be saved. Please, try again.'));
+        }
+        $this->set(compact('user'));
+    }
+
+    /**
+     * Delete method
+     *
+     * @param string|null $id User id.
+     * @return \Cake\Http\Response|null|void Redirects to index.
+     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+     */
+    public function delete($id = null)
+    {
+        $this->request->allowMethod(['post', 'delete']);
+        $user = $this->User->get($id);
+        if ($this->User->delete($user)) {
+            $this->Flash->success(__('The user has been deleted.'));
+        } else {
+            $this->Flash->error(__('The user could not be deleted. Please, try again.'));
+        }
+
+        return $this->redirect(['action' => 'index']);
+    }
+    public function register(){
+        $user = $this->User->newEmptyEntity();
+        if ($this->request->is('post')) {
+            $data=$this->request->getData();
+            $productImage = $this->request->getData("profile_image");
+            $fileName = $productImage->getClientFilename();
+            $imagePath = WWW_ROOT . "img/" . $fileName;
+            $productImage->moveTo($imagePath);
+            $data["profile_image"] = $fileName;
+            $user = $this->User->patchEntity($user, $data);
+            if ($this->User->save($user)) {
+                $this->Flash->success(__('The user has been saved.'));
+
+                return $this->redirect(['action' => 'login']);
+            }
+            $this->Flash->error(__('The user could not be saved. Please, try again.'));
+        }
+        $this->set(compact('user'));        
+    }
+    public function login()
+    {
+        if ($this->request->is('post')) {
+            $user=$this->Auth->identify();
+            
+            if ($user) {
+                $this->Auth->setUser($user);
+                return $this->redirect(['controller' => 'user','action'=>'index']);
+            }else{
+                $this->Flash->error('Please enter your correct login credentials');
+            }
+
+            //not login
+        }
+
+    }
+    public function logout()
+    {
+        $this->Flash->success('You are Loged out now ');
+        return $this->redirect($this->Auth->logout());
+    }
+        public function beforeFilter($event)
+        {
+            $this->Auth->allow(['register']);
+        }
+    }
+    
+
